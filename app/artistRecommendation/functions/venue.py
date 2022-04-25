3d2f3ee7459659d5598df7b8391a75f2804808e1
palak <palak2697@gmail.com>
2022-04-25 14:55:01 +0530

    sc-1295 chanes in venue fn

--- app/artistRecommendation/functions/venue.py
+++ app/artistRecommendation/functions/venue.py
@@ -1,18 +1,29 @@
 import pandas as pd
+pd.set_option('mode.chained_assignment', None)
 
-def venue(venues,v):
-    find_name = venues[venues['name'] == v]
-    find_id = find_name['id'].values[0]
-    return find_id
+def venue(v,venues):
+    list1 = []
+    for i in range(len(v)):
+        final_result1 = v[i]['name']
+        find_name = venues[venues['name'] == final_result1]
+        find_id = find_name['id'].values[0]
+        list1.append(find_id)
+
+    return list1  
 
 def event(e,events):
-    eve = events[events['venue_id'] == e]
-    return eve
+    data = pd.DataFrame()
+    for i in range(len(e)):
+        eve = events[events['venue_id'] == e[i]]
+        data = data.append(eve)
 
-def recommend_venue_1(df1,genre_name1,genres,events,venues):
+    return data
+
+
+def recommend_venue(df1,genre_name1,genres,events,venues):
     col_names = ['name_x' ,'popularity_1' , 'genres1','female','male','approx_budget','rating']
-    data = pd.DataFrame(columns = col_names)
-    find_events =event(venue(venues,df1),events)
+    data_frame = pd.DataFrame(columns = col_names)
+    find_events =event(venue(df1,venues),events)
     if len(find_events.columns) > 0:
         merge_1 = pd.merge(find_events, genre_name1, how='inner', on=['artist_id'])
         merge_2 = merge_1[['artist_id' , 'popularity_1' ,'female','male', 'approx_budget' ]]
@@ -27,11 +38,11 @@ def recommend_venue_1(df1,genre_name1,genres,events,venues):
             product_1=result_4.sort_values(by='popularity_1',ascending=False)
             product_2 = product_1[['name_x' , 'popularity_1' , 'genres1' ,'female','male' ,'approx_budget']]
             product_2['rating'] = product_2['popularity_1']*0.4
-            data = data.append(product_2)
+            data_frame = data_frame.append(product_2)
     else:
-        return data
+        return data_frame
     
-    return data
+    return data_frame
 
 
 
