da7474a7b21e4987851ccebb6f01cb14de70c7e3
palak <palak2697@gmail.com>
2022-04-26 17:33:55 +0530

    sc-1297 update in recommendation functions

--- app/artistRecommendation/functions/genre.py
+++ app/artistRecommendation/functions/genre.py
@@ -1,15 +1,25 @@
 import pandas as pd
-
-def genre(i,genre_artist):
-    list_artist = genre_artist[genre_artist['genre'] == i]
-    return list_artist 
+pd.set_option('mode.chained_assignment', None)
 
 
-def from_genre(genre1,genre_name1,events,genre_artist):
-    list_of_genre=genre(genre1,genre_artist)
-    list1=list_of_genre['artist_id'].tolist()
-    find_artist=events[events['artist_id'].isin(list1)]
-    recommend_1 = pd.merge(find_artist, genre_name1, how='inner', on=['artist_id'])
-    recommend_1['rating'] = recommend_1['popularity_1']*0.5
-    recommend_1 = recommend_1[[ 'artist_id','name_x'  , 'popularity_1', 'genres1','approx_budget' , 'rating']]
-    return recommend_1
+def find_genre(body,genre ):
+    genre_list = []
+    for i in range(len(body['targetAudience']['genre'])):
+        genre_id = genre[genre['name'] == body['targetAudience']['genre'][i]['genreName']]
+        if len(genre_id)>0:
+            genre1 = genre_id['id'].values[0]
+            genre_list.append(genre1)
+            return genre_list
+    
+    return genre_list
+
+def find_genre_artist(body , genre , genre_event):
+    artist_list = pd.DataFrame()
+    list2 = find_genre(body ,genre)
+    for i in list2:
+        genre2 = genre_event[genre_event['genre_id'] == i]
+        artist_list = artist_list.append(genre2)
+        
+    return artist_list
+
+
--- app/artistRecommendation/repository/inmemory/__init__.py
+++ app/artistRecommendation/repository/inmemory/__init__.py
@@ -1,22 +1,29 @@
 from app.utils.getLocalCsv import getLocalCsv
 from app.artistRecommendation.functions import recommendation
+import pandas as pd
 
 class ArtistRecommendationInMemoryRepo:
     def __init__(self, connection):
         self.connection = connection
     
     def getRecommendation(self, data):
-        # df = getLocalCsv("genre_artist.csv")
-        genre_artist = getLocalCsv('final_sorted_genres.csv')
-        genre_artist = genre_artist[['artist_id', 'name', 'genre']]
-        events = getLocalCsv('sorted_events.csv')
-        genre_name = getLocalCsv('sorted_bowie_spotify.csv')
-        genre_name1 = genre_name[['artist_id' , 'name' , 'popularity_1' ,'genres1' , 'male' , 'female']]
-        venues = getLocalCsv('venues.csv')
-        genres = getLocalCsv('genre_artist.csv')
-        genre_list = getLocalCsv('sorted_genres.csv')
-        genre_list['genre'] = genre_list['genre'].str.replace("'", '')
-        output = recommendation(data, genre_name1,genres,events,venues,genre_artist)
+        venues_csv = getLocalCsv('venues.csv')
+        sorted_events = getLocalCsv('sorted_events.csv')
+        sorted_genres = getLocalCsv('sorted_genres.csv')
+        final_sorted_genres = getLocalCsv('final_sorted_genres.csv')
+        sorted_bowie_spotify =  getLocalCsv('sorted_bowie_spotify.csv')
+        sorted_bowie_spotify = sorted_bowie_spotify[['artist_id' , 'name' , 'popularity_1']] 
+        sorted_events1 = pd.merge(sorted_events,sorted_bowie_spotify , how = 'inner' , on = 'artist_id')
+        final_sorted_genres = final_sorted_genres.rename(columns={'name': 'artist_name', 'genre' : 'name'})
+        sorted_genres1 = sorted_genres.rename(columns={'Unnamed: 0': 'id', 'genre' : 'name'})
+        list_genre = pd.merge(sorted_genres1 , final_sorted_genres , how = 'inner' , on = 'name')
+        event_artist_csv = sorted_events1[['id','artist_id' , 'venue_id','popularity_1' ,'approx_budget']]
+        event_artist_csv = event_artist_csv.rename(columns={'popularity_1' : 'popularity'})
+        genre_events_csv = pd.merge(list_genre , event_artist_csv , how = 'inner' , on = 'artist_id')
+        genre_events_csv = genre_events_csv.rename(columns={'id_x': 'genre_id', 'id_y' : 'id' })
+        genre_events_csv = genre_events_csv[['artist_id','popularity', 'genre_id', 'id','venue_id','approx_budget']]
+
+        output = recommendation(data , event_artist_csv , genre_events_csv,venues_csv,sorted_genres1)
         final_result =  output.to_json(orient = 'records' , lines=True).replace('\n' , ' ')
         
         return final_result
