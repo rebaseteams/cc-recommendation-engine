aab9648fc969c0e0bf1183307c8256f068fadde5
palak <palak2697@gmail.com>
2022-04-26 17:33:55 +0530

    sc-1297 update in recommendation functions

--- app/artistRecommendation/functions/__init__.py
+++ app/artistRecommendation/functions/__init__.py
@@ -1,31 +1,22 @@
-from app.artistRecommendation.functions.venue import   venue , recommend_venue_1
-from app.artistRecommendation.functions.genre import genre , from_genre
+from app.artistRecommendation.functions.venue import   find_venues , find_artist_from_venue
+from app.artistRecommendation.functions.genre import find_genre , find_genre_artist
 import pandas as pd
 import json
 
 
-def recommendation(body , genre_name1,genres,events,venues,genre_artist):
-    df=json.dumps(body)
-    train = pd.read_json(df , orient = 'index')
-    df1 = train.T
-    print(df1)
-    df2 = df1.explode('genre' , ignore_index= True)
-    df2['approx budget'] = df2['approx budget'].astype(int)
-    recommend_from_venue=recommend_venue_1(df1['venue'][0],genre_name1,genres,events,venues)
-    column = [ 'artist_id','name_x'  , 'popularity_1', 'genres1','approx_budget' , 'rating']
-    genre_list1 = pd.DataFrame(columns = column)
-    list1 = df2['genre'].to_list()
-    for i in list1:
-        find_artists = from_genre(i,genre_name1,events,genre_artist)
-        genre_list1=genre_list1.append(find_artists)
-
-    frames = [genre_list1,recommend_from_venue]
-    result = pd.concat(frames)
-    result['approx_budget'] = result['approx_budget'].astype(int)
-    frame_1 = result[result['approx_budget'].between(int(float(10000)) , int(df2['approx budget'][0]))]
-    idx = frame_1.groupby(['name_x'])['approx_budget'].transform(max) == frame_1['approx_budget']
+def recommendation(abc ,event_artist,genre_event,venue_db,genre):
+    venues_list=find_artist_from_venue(find_venues(abc , venue_db), event_artist)
+    genre_list=find_genre_artist(abc, genre , genre_event)
+    ab = pd.concat([venues_list , genre_list])
+    list1=list(set(ab['artist_id'].tolist()))
+    data1 = genre_event[genre_event['artist_id'].isin(list1)]
+    list2=list(set(data1['genre_id'].tolist()))
+    data2 = genre_event[genre_event['genre_id'].isin(list2)]
+    frame_1 = data2[data2['approx_budget'].between(int(abc['artistBudget']['min']) , int(abc['artistBudget']['max']))]
+    idx = frame_1.groupby(['artist_id'])['approx_budget'].transform(max) == frame_1['approx_budget']
     max1=frame_1[idx]
-    max2 = max1.sort_values(by='rating',ascending=False , ignore_index = True)
-    max3 = max2[['name_x' ,'popularity_1' ,'genres1']]
-    max3 = max3.drop_duplicates(subset = 'name_x',ignore_index = True)
-    return max3.head(6)  
+    max2 = max1.sort_values(by='popularity',ascending=False , ignore_index = True)
+    max3 = max2.drop_duplicates(subset = 'artist_id',ignore_index = True)
+    return max3
+
+
--- app/artistRecommendation/functions/venue.py
+++ app/artistRecommendation/functions/venue.py
@@ -1,48 +1,24 @@
 import pandas as pd
 pd.set_option('mode.chained_assignment', None)
 
-def venue(v,venues):
+def find_venues(v,venue):
     list1 = []
-    for i in range(len(v)):
-        final_result1 = v[i]['name']
-        find_name = venues[venues['name'] == final_result1]
+    for i in range(len(v['venue'])):
+        find_name = venue[venue['name'] == v['venue'][i]['name']]
         find_id = find_name['id'].values[0]
         list1.append(find_id)
 
     return list1  
 
-def event(e,events):
-    data = pd.DataFrame()
-    for i in range(len(e)):
-        eve = events[events['venue_id'] == e[i]]
-        data = data.append(eve)
 
+def find_artist_from_venue(v ,event_artist):
+    data = pd.DataFrame()
+    for i in v:
+        list_artist = event_artist[event_artist['venue_id'] == i]
+        data = data.append(list_artist)
+        
     return data
 
-
-def recommend_venue(df1,genre_name1,genres,events,venues):
-    col_names = ['name_x' ,'popularity_1' , 'genres1','female','male','approx_budget','rating']
-    data_frame = pd.DataFrame(columns = col_names)
-    find_events =event(venue(df1,venues),events)
-    if len(find_events.columns) > 0:
-        merge_1 = pd.merge(find_events, genre_name1, how='inner', on=['artist_id'])
-        merge_2 = merge_1[['artist_id' , 'popularity_1' ,'female','male', 'approx_budget' ]]
-        merge_3=merge_2.sort_values(by='popularity_1',ascending=False)
-        merge_3 = merge_3.drop_duplicates(subset = 'artist_id', ignore_index = True)
-        for i in range(len(merge_3)):
-            merge_4=merge_3['artist_id'].values[i]
-            result_1=genres[genres['artist_id'] == merge_4]['genre'].values[0]
-            result_2=genres[genres['genre'] == result_1]
-            result_3 = pd.merge(result_2, genre_name1 ,how='inner', on = ['artist_id'])
-            result_4 = pd.merge(result_3 , events , how = 'inner' , on = ['artist_id'])
-            product_1=result_4.sort_values(by='popularity_1',ascending=False)
-            product_2 = product_1[['name_x' , 'popularity_1' , 'genres1' ,'female','male' ,'approx_budget']]
-            product_2['rating'] = product_2['popularity_1']*0.4
-            data_frame = data_frame.append(product_2)
-    else:
-        return data_frame
-    
-    return data_frame
 
 
 
